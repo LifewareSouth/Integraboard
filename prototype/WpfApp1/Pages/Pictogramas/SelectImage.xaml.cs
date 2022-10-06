30c449d84b02489aac548c2d3f94a2d2bdc6235f
amunozfuentes <a.munoz@lifeware.cl>
2022-10-06 16:48:01 -0300

    vista que faltaba para agregar imagen desde el banco de imagenes o cargar imagen nueva

---
+++ prototype/WpfApp1/Pages/Pictogramas/SelectImage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Pages.Pictogramas
+{
+    /// <summary>
+    /// Lógica de interacción para SelectImage.xaml
+    /// </summary>
+    public partial class SelectImage : Page
+    {
+        public SelectImage()
+        {
+            InitializeComponent();
+        }
+
+        private void AddImages_Click(object sender, RoutedEventArgs e)
+        {
+            //Aquí va lo que estaba en la otra page
+            string pathImagen = null;
+            Microsoft.Win32.OpenFileDialog ofdImage = new OpenFileDialog();
+            ofdImage.Filter = "Imagenes (*.BMP;*.JPG;*.PNG)|*.BMP;*.JPG;*.PNG";
+            bool? response = ofdImage.ShowDialog();
+            foreach (string name in ofdImage.SafeFileNames)
+            {
+                pathImagen = ofdImage.FileNames.Where(stringToCheck => stringToCheck.Contains(name)).First();
+            }
+            //PictogramImage.Source = LoadBitmapImage(pathImagen);
+        }
+    }
+}
