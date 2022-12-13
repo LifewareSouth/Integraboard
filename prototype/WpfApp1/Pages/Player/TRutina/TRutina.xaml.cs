ca6b580ad534a5254fbe8a6d8c19ed5756cd5f46
tespinozalw <t.espinoza@lifeware.cl>
2022-12-13 17:29:20 -0300

    Datos de tablero en tablero rutina

--- prototype/WpfApp1/Pages/Player/TRutina/TRutina.xaml.cs
+++ prototype/WpfApp1/Pages/Player/TRutina/TRutina.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -24,7 +25,9 @@ namespace WpfApp1.Pages.Player.TRutina
     public partial class TRutina : Page
     {
         private static BitmapImage imagenBoton = Repository.Instance.getImageFromResources(WpfApp1.Properties.Resources.playButtonBlanco);
+        private BindingList<pictTablero> vistas = new BindingList<pictTablero>();
         List<pictTablero> ListaPict = new List<pictTablero>();
+        int rowCounter, columnsCounter;
         public TRutina()
         {
             InitializeComponent();
@@ -34,9 +37,65 @@ namespace WpfApp1.Pages.Player.TRutina
         public TRutina(Board board)
         {
             InitializeComponent();
+            rowCounter = board.filas;
+            columnsCounter = board.columnas;
             ListaPict = board.pictTableros;
-            //bind aqui con Tablero.source = ListaPict
+            Tablero.ItemsSource = board.pictTableros;
             this.Resources["check"] = imagenBoton;
+        }
+        private void page_Loaded(object sender, RoutedEventArgs e)
+        {
+            ajustarTamano();
+        }
+        private void ajustarTamano()
+        {
+            UniformGrid foundUniformGrid = FindVisualChild<UniformGrid>(Tablero);
+            foundUniformGrid.Columns = columnsCounter;
+            foundUniformGrid.Rows = rowCounter;
+        }
+        private T FindVisualChild<T>(DependencyObject obj) where T : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                var child = VisualTreeHelper.GetChild(obj, i);
+                if (child != null && child is T)
+                    return (T)child;
+                var childOfChild = FindVisualChild<T>(child);
+                if (childOfChild != null)
+                    return childOfChild;
+            }
+            return null;
+        }
+        private void AjustarTablero()
+        {
+            int totalCuadros = rowCounter * columnsCounter;
+            BindingList<pictTablero> tempVistas = new BindingList<pictTablero>();
+            for (int i = 0; i < rowCounter; i++)
+            {
+                for (int j = 0; j < columnsCounter; j++)
+                {
+                    pictTablero pictTab = new pictTablero();
+                    if (ListaPict.Any(x => (x.x == j) && (x.y == i)))
+                    {
+                        pictTab = ListaPict.Where(x => (x.x == j) && (x.y == i)).First();
+                    }
+                    else
+                    {
+
+                        Pictogram tempPictograma = new Pictogram();
+                        tempPictograma.colorBorde = new SolidColorBrush(Colors.White);
+                        tempPictograma.Texto = "";
+                        pictTab.pictograma = tempPictograma;
+
+                    }
+                    pictTab.x = j;
+                    pictTab.y = i;
+                    tempVistas.Add(pictTab);
+
+                }
+            }
+            vistas = tempVistas;
+            Tablero.ItemsSource = vistas;
         }
 
         private void playButton_Click(object sender, RoutedEventArgs e)
