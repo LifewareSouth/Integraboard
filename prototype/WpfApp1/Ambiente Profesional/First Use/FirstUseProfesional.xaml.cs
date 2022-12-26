808a5b9a80b87d3005392a1f02e29d525db60667
amunozfuentes <a.munoz@lifeware.cl>
2022-12-26 19:58:05 -0300

    desarrollo del ambiente del profesional

---
+++ prototype/WpfApp1/Ambiente Profesional/First Use/FirstUseProfesional.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Ambiente_Profesional.First_Use
+{
+    /// <summary>
+    /// Lógica de interacción para FirstUseProfesional.xaml
+    /// </summary>
+    public partial class FirstUseProfesional : Page
+    {
+        public FirstUseProfesional()
+        {
+            InitializeComponent();
+        }
+    }
+}
