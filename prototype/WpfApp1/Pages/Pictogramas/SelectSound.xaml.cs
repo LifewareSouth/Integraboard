823559b1e01bd2be77c9e0cc9a994afc0ae12573
tespinozalw <t.espinoza@lifeware.cl>
2023-01-12 17:32:42 -0300

    actualizacion de lista de sonidos al agregar nuevo

--- prototype/WpfApp1/Pages/Pictogramas/SelectSound.xaml.cs
+++ prototype/WpfApp1/Pages/Pictogramas/SelectSound.xaml.cs
@@ -86,7 +86,7 @@ namespace WpfApp1.Pages.Pictogramas
                 string nombreSonidoNuevo = System.IO.Path.GetFileNameWithoutExtension(pathSonidoNuevo);
                 Repository.Instance.CrearSonido(pathSonidoNuevo, nombreSonidoNuevo, false);
 
-                //CODIGO PARA ACTUALIZAR LISTADO DE LA PAGINA ACTUAL
+                actualizarListaSonidos();
             }
         }
 
