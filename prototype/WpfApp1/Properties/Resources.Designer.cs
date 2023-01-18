6a3853b713f4eef60cee30e967bf6195d67358eb
amunozfuentes <a.munoz@lifeware.cl>
2023-01-18 12:19:47 -0300

    imagen pausar

--- prototype/WpfApp1/Properties/Resources.Designer.cs
+++ prototype/WpfApp1/Properties/Resources.Designer.cs
@@ -223,6 +223,16 @@ namespace WpfApp1.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap pausarConTexto {
+            get {
+                object obj = ResourceManager.GetObject("pausarConTexto", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap playButtonBlanco {
             get {
                 object obj = ResourceManager.GetObject("playButtonBlanco", resourceCulture);
