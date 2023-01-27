339589da5b7073b92f4ea8d9f5ebfd2ff9a93acf
BenjaminSerrano <jamon_1996@hotmail.com>
2023-01-27 15:42:43 -0300

    Fixed file to create a ci/cd

--- prototype/LifewareSoftwareCommon/GlobalConfig.cs
+++ prototype/LifewareSoftwareCommon/GlobalConfig.cs
@@ -14,11 +14,11 @@ namespace Lifeware.SoftwareCommon
         //TODO: Establecer nombre de aplicación aca
         private const string APPNAME = "prototipo";
         //TODO: Establecer versión de aplicación. Recordar actualizar en cada Release
-        public const string APPVERSION = "0.0.0.1";
+        public const string APPVERSION = "1.0.0";
         //TODO: Establecer GUID de la aplicación.
         private const string APPGUID = "c914f1b1-d3be-4266-9730-ed6942f3cea9";
         //TODO: Establecer URL de descargas y actualizaciones de la aplicación
-        private const string UPDATE_URL = "http://updates.lifeware.cl/integraboardStandAlone/testing/";
+        private const string UPDATE_URL = "http://updates.lifeware.cl/integraboard4/testing/";
         
         //no tocar desde este punto
 
