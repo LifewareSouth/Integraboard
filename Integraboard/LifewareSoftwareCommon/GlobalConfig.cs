16fdcc27cb686f15099d263fe5267f5bc30af012
tespinozalw <t.espinoza@lifeware.cl>
2023-03-24 12:43:26 -0300

    cambio appname y appguid

--- Integraboard/LifewareSoftwareCommon/GlobalConfig.cs
+++ Integraboard/LifewareSoftwareCommon/GlobalConfig.cs
@@ -12,11 +12,11 @@ namespace Lifeware.SoftwareCommon
     public class GlobalConfig
     {
         //TODO: Establecer nombre de aplicación aca
-        private const string APPNAME = "Integraboard";
+        private const string APPNAME = "IntegraBoard_Stand_Alone";
         //TODO: Establecer versión de aplicación. Recordar actualizar en cada Release
         public const string APPVERSION = "1.0.1";
         //TODO: Establecer GUID de la aplicación.
-        private const string APPGUID = "c914f1b1-d3be-4266-9730-ed6942f3cea9";
+        private const string APPGUID = "0ec6ed26-b1b8-4906-acdf-dd05a3901f88";
         //TODO: Establecer URL de descargas y actualizaciones de la aplicación
         private const string UPDATE_URL = "http://updates.lifeware.cl/integraboard4/testing/";
         
